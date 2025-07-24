44e48d74e93e35a8e9dd646716d02bb7dfe4a311
Kulkarni <pallavi.e.kulkarni@capgemini.com>
2025-07-24 13:59:03 +0200

    Testnew  change new try1 change

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -17,13 +17,5 @@ test('Open Cooffeeshop page and verify title is same --todays test', async ({ pa
     await page.goto('https://marina-abr.github.io/StaticCoffee/index.html');
    //verify the title of the page  
     await expect(page).toHaveTitle('coffeeshop');
-});
-
-
-test('Open Cooffeeshop page and verify error', async ({ page }) => {
-    //Open URL
-    await page.goto('https://marina-abr.github.io/StaticCoffee/index.html');
-   //verify the title of the page  
-    await expect(page).toHaveTitle('coffeeshop');
-});
+})
 });
